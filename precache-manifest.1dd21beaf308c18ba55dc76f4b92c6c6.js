c784d534a831d224d2d0d96e08d32288206c8786
chandrika <chandrikabiswal1@gmail.com>
2020-04-28 00:45:00 +0530

    Updates

---
+++ precache-manifest.1dd21beaf308c18ba55dc76f4b92c6c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d67517b65e364024aca2181a55b00b",
+    "url": "/advice-app/index.html"
+  },
+  {
+    "revision": "413281ac83ca46853063",
+    "url": "/advice-app/static/css/main.af3634ab.chunk.css"
+  },
+  {
+    "revision": "34a19ce9dd76bec6e437",
+    "url": "/advice-app/static/js/2.4b3513ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advice-app/static/js/2.4b3513ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413281ac83ca46853063",
+    "url": "/advice-app/static/js/main.42d7e87c.chunk.js"
+  },
+  {
+    "revision": "127ba45e1c40ce663e8a",
+    "url": "/advice-app/static/js/runtime-main.bc40df97.js"
+  }
+]);
